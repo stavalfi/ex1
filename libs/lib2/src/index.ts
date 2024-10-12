0de11f767e66a389aa7cc7c8d1ca7e8c1527067f
Stav Alfi <stava@papaya.com>
2024-10-12 15:27:07 +0300

    wip

--- libs/lib2/src/index.ts
+++ libs/lib2/src/index.ts
@@ -1,4 +1,3 @@
 export const lib2 = 2
 
-const z1:string = 1
 const z2:string = null
--- service/src/index.ts
+++ service/src/index.ts
@@ -1,7 +1,6 @@
 import {lib1} from '@p/lib1'
 import {lib2} from '@p/lib2'
 
-const y1:string = 1
-const y2:string = null
+// const y2:string = 1
 
 console.log("stav1",{lib1,lib2})
